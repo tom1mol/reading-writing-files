d5c3566610c806fc031260fd8c22483ce2753e03
tom1mol <tomasomaolmhuaidh@gmail.com>
2018-11-27 11:55:30 +0000

    quiz game walkthrough 1

--- quiz/quiz.py
+++ quiz/quiz.py
@@ -6,4 +6,35 @@ def show_menu():                        #function called show menu
     option = input("Enter Option: ")
     return option
     
-print(show_menu())
+def add_question():
+    print("")                                #print a blank line
+    question = input("Enter a question\n> ")        #question prompt
+    
+    print("")                                   #print blank line
+    print("OK then, tell me the answer")
+    answer = input("{0}\n> ".format(question))  #{0} take question we already asked. blank line. greater than prompt
+                #using formaT method we insert question. user asked question when they go to put answer in
+                
+    file = open("questions.txt", "a")   #open questions.txt for appending
+    file.write(question + "\n")         # write question with new line at end
+    file.write(answer + "\n")           #answer goes on own line
+    file.close()
+    
+    
+#print(show_menu())  we had this to check the code worked
+def game_loop():
+    while True:     #ie loop forever unless theres a break
+        option = show_menu()        #call show_menu function and store choice in option variable 
+        if option == "1":
+            print("You selected 'Ask questions'")
+        elif option == "2":         #use cat questions.txt to look at the contents of questions.txt
+            add_question()                        #print("you selected 'Add a question'")
+        elif option == "3":
+            break
+        else:
+            print("Invalid option")
+        print("")
+        
+game_loop()
+        
+        
